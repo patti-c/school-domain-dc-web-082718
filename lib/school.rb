42dfcb6a1791315693bc42d1bc038548d487c06f
patti-c <patricia.carter@flatironschool.com>
2018-08-29 16:48:43 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade) == false
+      @roster[grade] = []
+    end
+
+    @roster[grade] << name
+
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, names|
+      @roster[grade] = names.sort
+    end
+    return @roster
+  end
+
+end
